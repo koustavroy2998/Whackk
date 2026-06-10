910c696282a63a80cd5368971b90ba0cc2d62c51
koustavroy2998 <koustav.roy@stancebeam.com>
2026-06-10 13:41:06 +0530

    Updated lottie color

--- app/src/main/java/com/stancebeam/whackk/ui/screen/webview/WebViewState.kt
+++ app/src/main/java/com/stancebeam/whackk/ui/screen/webview/WebViewState.kt
@@ -3,7 +3,7 @@ package com.stancebeam.cc_lane.ui.screen.webview
 data class WebViewState(
     var isLoading: Boolean = false,
     var showConnectionSheet: Boolean = false,
-    val url: String = "http://192.168.0.218:5176"// main hosted url
+    val url: String = "http://192.168.0.192:5176"// main hosted url
     //val url: String = "http://192.168.0.91:5176/?facilityCode=BLR01"// dev hosted url
   // val url: String = "http://172.16.10.2:8080/?facilityCode=HOU01"// US URL;[
   // val url: String = "https://www.stancebeam.com/"// US URL;[
